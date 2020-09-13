cdcd46ed24a5c897476d6572852015c13259f502
陌生人 <moshengrenx@foxmail.com>
2020-09-13 15:29:28 +0800

    使用jq去和合并json

--- refresh.sh
+++ refresh.sh
@@ -22,24 +22,7 @@ do
         cd $i #进入目录
         if [ -f 'applist.json' ];then #如果存在特定文件
             rm applist.json #删除
-            echo "[" >> applist.json #写入数组的左边
-            isFind="0" #设置标记，用来处理空文件
-            for j in `find . -name '*.json' -type f` #查找所有的json文件
-            do
-                if [ -s $j ] ; then #如果文件存在切不为空
-                    result=$(echo $j | grep "applist.json") #判断是否包含applist.json
-                    if [ "$result" == "" ] ;then #不包含
-                        isFind="1" #不是空文件
-                        cat $j >>applist.json #json写入文件
-                        echo ",">> applist.json #用逗号分隔
-                    fi
-                fi
-            done
-
-            if [ $isFind -eq "1" ];then #判断找到的文件是否是全空的
-                sed -i '$d' applist.json #删除最后一行的逗号
-            fi
-            echo "]">> applist.json #写入右半部分
+            cat `find . -name 'app.json' -type f`|jq -s > applist.json #查找所有的json文件
             echo "商店信息写入完毕"
         fi
         cd ..
