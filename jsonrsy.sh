9027a2a3fbea41f8fc9e4f5a91b2e61794cf3dc1
Jerry <767729940@qq.com>
2020-10-01 00:09:35 +0800

    !3 国际化json仓库所需的json同步更改 * 增加对更新app.json的判定 * applist.json  applist.en.json在json目录下生成 * 合并来自软件仓库的json和gitee上pr的json

--- jsonrsy.sh
+++ jsonrsy.sh
@@ -1,18 +1,54 @@
-# 单纯同步json到5M主
+#json同步脚本
 FROM_DIR=/home/ftp/deepin-community-store/
 TO_DIR=/home/ftp/json/
 
-cd $FROM_DIR
+# 从gitee上拉去已合并的pr
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/jerrygithub
+git pull origin
+echo " 从gitee拉取完毕"
 
+# 从仓库更新json,从中文json生成英文
+cd $FROM_DIR
 find store -type d -exec mkdir -p $TO_DIR\{\} \;
 
 for i in `find store -iname "*.json" -type f` 
 do
-di=${i%/*}
-cp $i $TO_DIR$di -u
+		di=${i%/*}
+		filetimestamp_old=`stat -c %Y $TO_DIR$di/app.json`
+		cp $i $TO_DIR$di/app.json -u  # -u 表示只更新
+		filetimestamp_new=`stat -c %Y $TO_DIR$di/app.json`
+		timecha=$[$filetimestamp_new - $filetimestamp_old]
+		if [ $timecha -gt 60 ];then   #如果cp -u 前后两个文件的时间戳大于60s， 则说明app.json 发生了变动
+				  echo 'app.json信息发生变动'
+					cp $i $TO_DIR$di/app.en.json  #app.json更新则更新app.en.json
+		fi
 done
-
+echo "从仓库更新完毕"
 cd $TO_DIR
 
-rsync -rztP --delete-after /home/ftp/json/  spark@101.132.134.206::json --password-file=/etc/rsyncpasswd
+#生成 applist.json 和 applist.en.json
+cd store
+for i in `ls` #for循环遍历store目录下的文件
+do
+    if [ -d $i ] ; then #如果当前变量的是目录
+        cd $i #进入目录
+        echo $i
+				if [ -f 'applist.json' ];then
+            cat `find . -name 'app.json' -type f`|jq -s . > applist.json #查找所有的json文件
+            cat `find . -name 'app.en.json' -type f`|jq -s . > applist.en.json
+				fi
+        cd ..
+    fi
+done
+echo "商店信息写入完毕"
+date
 
+cd ..
+#合并仓库生成的新json和来自gitee仓库的pr推送到gitee
+git add .
+git commit -m "auto push"
+git push origin
+
+# 同步到下游
+rsync -rztP --delete-after /home/ftp/json/  spark@101.132.134.206::json --password-file=/etc/rsyncpasswd
