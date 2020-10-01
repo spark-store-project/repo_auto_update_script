6ec5e1248df9296cbe237455f1ff47e8c044cd31
Jerry <767729940@qq.com>
2020-10-01 10:55:55 +0800

    优化了从app.json到app.en.json的更新

--- jsonrsy.sh
+++ jsonrsy.sh
@@ -9,23 +9,25 @@ ssh-add ~/.ssh/jerrygithub
 git pull origin
 echo " 从gitee拉取完毕"
 
-# 从仓库更新json,从中文json生成英文
+# 从仓库更新json, 从中文json生成英文
 cd $FROM_DIR
 find store -type d -exec mkdir -p $TO_DIR\{\} \;
 
-for i in `find store -iname "*.json" -type f` 
+for i in `find store -iname 'app.json' -type f`
 do
-		di=${i%/*}
-		filetimestamp_old=`stat -c %Y $TO_DIR$di/app.json`
-		cp $i $TO_DIR$di/app.json -u  # -u 表示只更新
-		filetimestamp_new=`stat -c %Y $TO_DIR$di/app.json`
-		timecha=$[$filetimestamp_new - $filetimestamp_old]
-		if [ $timecha -gt 60 ];then   #如果cp -u 前后两个文件的时间戳大于60s， 则说明app.json 发生了变动
-				  echo 'app.json信息发生变动'
-					cp $i $TO_DIR$di/app.en.json  #app.json更新则更新app.en.json
-		fi
+di=${i%/*}
+filetimestamp_old=`stat -c %Y $TO_DIR$di/app.json`
+cp $i $TO_DIR$di/app.json -pu  # -u 表示只更新  -p 表示保留时间戳
+filetimestamp_new=`stat -c %Y $TO_DIR$di/app.json`
+timecha=$[$filetimestamp_new - $filetimestamp_old]
+echo $timecha
+if [ $timecha -gt 60 ];then   #如果cp -up 前后两个文件的时间戳大于60s， 则说明app.json 发生了变动
+echo 'app.json信息发生变动'
+cp $i $TO_DIR$di/app.en.json  #app.json更新则更新app.en.json
+fi
 done
 echo "从仓库更新完毕"
+
 cd $TO_DIR
 
 #生成 applist.json 和 applist.en.json
