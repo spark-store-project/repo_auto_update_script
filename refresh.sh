f55f6af29e00656236169e8402e1fc06253de2be
shenmo <jifengshenmo@outlook.com>
2022-06-24 04:01:29 +0000

    update refresh.sh.

--- refresh.sh
+++ refresh.sh
@@ -1,19 +1,9 @@
 REPOPATH="/home/ftp/spark-store" #设置软件源目录
 cd $REPOPATH #进入根目录
-echo "================================================"
-echo "星火刷新daemon守护启动，已经进入仓库目录，目录为"
-pwd #显示路径
-echo "守护模式已经开启，正在持续检测旗帜信号"
-echo "================================================"
-#生成文件
-#apt-ftparchive packages . > Packages
-#apt-ftparchive release . > Release
+pwd
 
-while true; do
-if [ -f "${REPOPATH}/refresh.flag" ];then
-#删除flag
-echo "检测到旗帜refresh.flag已经插入，收到信号，拔除旗帜"
-rm ${REPOPATH}/refresh.flag
+
+
 #进入update阶段
 echo "开始刷新第一阶段：处理旧式投稿工具的.update文件"
 find "${REPOPATH}" -name '*.update' -print0 | xargs -0 file-rename -f -v 's/\.update$//'
@@ -44,20 +34,11 @@ bash /root/jsonrsy.sh
 #同步 图片
 # 不再用 jsd 不需要同步了
 # bash /root/syn_jsonpic_jsd.sh
-touch ${REPOPATH}/finish-refresh.flag
 
-sleep 2
-echo "================================================"
-echo "刷新进程结束，插入finish-refresh.flag以便其他进程读取"
-echo "================================================"
-sleep 3
-reset
 echo "星火刷新daemon守护已完成刷新"
 echo "刷新时间为"
 date
-echo "已进入守护模式，正在持续检测旗帜信号"
+
 #以上来源pluto
-else 
-sleep 5
-fi
-done
+
+
