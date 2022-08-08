b1861c6917d4403aafebf683d2b6c757d3ba72c9
shenmo <jifengshenmo@outlook.com>
2022-08-08 00:58:10 +0000

    Fix path

--- spark-store-metalink/metalink.py
+++ spark-store-metalink/metalink.py
@@ -3,12 +3,12 @@ import json
 import xml.dom.minidom
 
 
-pacakges_file_path = '/mnt/d/workspace/spark/repo_auto_update_script/spark-store-metalink/Packages'
-torrent_file_path = '/mnt/d/workspace/spark/repo_auto_update_script/spark-store-metalink/torrent.json'
-cdn_file_path = '/mnt/d/workspace/spark/repo_auto_update_script/spark-store-metalink/server-and-mirror.list'
-output_base_dir = '/mnt/d/workspace/spark/repo_auto_update_script/spark-store-metalink/'
+pacakges_file_path = '/home/ftp/spark-store/Packages'
+torrent_file_path = '/home/ftp/spark-store/store/torrent.json'
+cdn_file_path = '/home/ftp/spark-store/store/server-and-mirror.list'
+output_base_dir = '/home/ftp/spark-store/store/'
 
-example_file_path = '/mnt/d/workspace/spark/repo_auto_update_script/spark-store-metalink/example.metalink'
+example_file_path = '/root/repo-scripts/repo_auto_update_script/spark-store-metalinkexample.metalink'
 http_xml_example = ' <url type="http" location="cn" preference="100">example_http</url> '
 bt_xml_example = ' <url type="bittorrent" preference="100">example_bt</url> '
 torrent_url = 'https://d.store.deepinos.org.cn/'
