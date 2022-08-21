123a1a0c967a44fd7ead664cd1f312660ffe09b4
shenmo <jifengshenmo@outlook.com>
2022-08-21 20:03:58 +0800

    修复杂项

--- spark-build.sh
+++ spark-build.sh
@@ -8,10 +8,33 @@ apt update
 echo "安装git devscripts equivs ..."
 apt install git devscripts equivs curl -y >/dev/null 2>&1
 
+mkdir build-dtk && cd build-dtk
+git clone https://gitee.com/deepin-community-store/spark-dtk
+cd spark-dtk
+
+
+cd libdtkcommon/dtkcommon-5.5.17 && mk-build-deps --install --tool "apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y"  && dpkg-buildpackage -b -us -uc
+cd ..
+apt install ./*.deb -y
+cd ..
+
+cd dtkcore/dtkcore-5.4.20 && mk-build-deps --install --tool "apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y" && dpkg-buildpackage -b -us -uc
+cd ..
+apt install ./*.deb -y
+cd .. 
+
+cd dtkgui/dtkgui-5.4.15 && mk-build-deps --install --tool "apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y" && dpkg-buildpackage -b -us -uc
+cd ..
+apt install ./*.deb -y
+cd .. 
+
+cd ../..
+
+rm -rf build-dtk
+
 git clone https://gitee.com/deepin-community-store/spark-store 
 cd spark-store
 
-apt install libdtkcore-dev libdtkgui-dev libdtkwidget-dev -y
 
 
 mk-build-deps --install --tool "apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y" 
