1acf1f04b10fa9f96f3fbd1617bfec2a576a1187
shenmo <jifengshenmo@outlook.com>
2022-08-22 03:26:30 +0000

    update spark-build.sh.
    Signed-off-by: shenmo <jifengshenmo@outlook.com>

--- spark-build.sh
+++ spark-build.sh
@@ -1,50 +1,22 @@
-
-
-
-mkdir build-spark
-cd build-spark
+sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+# 换源
 apt update
-
+export DEBIAN_FRONTEND=noninteractive
 echo "安装git devscripts equivs ..."
 apt install git devscripts equivs curl -y >/dev/null 2>&1
-
-mkdir build-dtk && cd build-dtk
-git clone https://gitee.com/deepin-community-store/spark-dtk
-cd spark-dtk
-
-
-cd libdtkcommon/dtkcommon-5.5.17 && mk-build-deps --install --tool "apt-get -o Debug::pkgProblemResolver=yes -y"  && dpkg-buildpackage -b -us -uc
-cd ..
+git clone https://gitlink.org.cn/shenmo7192/dtk-old-bundle.git
+cd dtk-old-bundle
 apt install ./*.deb -y
 cd ..
+rm -rf dtk-old-bundle
+mkdir build-spark
+cd build-spark
 
-cd dtkcore/dtkcore-5.4.20 && mk-build-deps --install --tool "apt-get -o Debug::pkgProblemResolver=yes  -y" && dpkg-buildpackage -b -us -uc
-cd ..
-apt install ./*.deb -y
-cd .. 
-
-
-cd dtkgui/dtkgui-5.4.15 && mk-build-deps --install --tool "apt-get -o Debug::pkgProblemResolver=yes  -y" && dpkg-buildpackage -b -us -uc
-cd ..
-apt install ./*.deb -y
-cd .. 
-
-cd dtkwidget/dtkwidget-5.4.20 && mk-build-deps --install --tool "apt-get -o Debug::pkgProblemResolver=yes -y" && dpkg-buildpackage -b -us -uc
-cd ..
-apt install ./*.deb -y
-cd .. 
-
-
-cd ../..
-
-rm -rf build-dtk
-
-git clone https://gitee.com/deepin-community-store/spark-store 
+git clone https://gitee.com/deepin-community-store/spark-store
 cd spark-store
-
-
-
 mk-build-deps --install --tool "apt-get -o Debug::pkgProblemResolver=yes  -y" 
 dpkg-buildpackage -b -us -uc
+cd ..
+ls -all
+pwd
 
-curl -s --url "smtp://smtp.163.com" --mail-from "sparkstorefeedback@163.com" --mail-rcpt "shenmo@spark-app.store" --upload-file ./*.deb --user "sparkstorefeedback@163.com:YWYGLQNOPLWNNJJY"
