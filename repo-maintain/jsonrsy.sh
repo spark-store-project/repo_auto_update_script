0063f151ae111e1c34aa80769332b698747daf1b
shenmo <jifengshenmo@outlook.com>
2022-09-17 16:08:42 +0000

    update repo-maintain/jsonrsy.sh.
    Signed-off-by: shenmo <jifengshenmo@outlook.com>

--- repo-maintain/jsonrsy.sh
+++ repo-maintain/jsonrsy.sh
@@ -19,26 +19,18 @@ di=${i%/*}
 cp $i $TO_DIR$di/app.json -u  # -u 表示只有当源文件比目标文件新(或者目标文件不存在)时，才会更新，这避免了冲突（我之前居然没有意识到）
 cp $i $TO_DIR$di/en.app.json -u
 done
+
+for i in `find store -iname 'applist.json' -type f`
+do
+di=${i%/*}
+cp $i $TO_DIR$di/applist.json -u  # -u 表示只有当源文件比目标文件新(或者目标文件不存在)时，才会更新，这避免了冲突（我之前居然没有意识到）
+done
+
 echo "从仓库更新完毕"
 
 cd $TO_DIR
 
-#生成 applist.json 和 en.applist.json
-cd store
-for i in `ls` #for循环遍历store目录下的文件
-do
-    if [ -d $i ] ; then #如果当前变量的是目录
-        cd $i #进入目录
-        echo $i
-				if [ -f 'applist.json' ];then
-            cat `find . -name 'app.json' -type f`|jq -s . > applist.json #查找所有的json文件
-            cat `find . -name 'en.app.json' -type f`|jq -s . > en.applist.json
-				fi
-        cd ..
-    fi
-done
-echo "商店信息写入完毕"
-date
+
 
 cd ..
 #合并仓库生成的新json和来自gitee仓库的pr推送到gitee
