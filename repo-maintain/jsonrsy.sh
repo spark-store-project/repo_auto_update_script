f1388f87a3ca5e8cc14c1a774597853a460f9e56
shenmo <jifengshenmo@outlook.com>
2023-04-25 14:30:30 +0000

    update repo-maintain/jsonrsy.sh.
    Signed-off-by: shenmo <jifengshenmo@outlook.com>

--- repo-maintain/jsonrsy.sh
+++ repo-maintain/jsonrsy.sh
@@ -2,59 +2,19 @@
 FROM_DIR=/home/ftp/spark-store/
 TO_DIR=/home/ftp/json/
 
-# 从gitee上拉去已合并的pr
 cd $TO_DIR
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/jerrygithub
 git pull origin
-echo " 从gitee拉取完毕"
 
-# 从仓库更新json, 从中文json生成英文
-cd $FROM_DIR
+rsync -av --delete --include='*.json' --exclude='*' $FROM_DIR $TO_DIR
 
 
-find store -type d -exec mkdir -p $TO_DIR\{\} \;
-
-for i in `find store -iname 'app.json' -type f`
-do
-di=${i%/*}
-cp $i $TO_DIR$di/app.json -u  # -u 表示只有当源文件比目标文件新(或者目标文件不存在)时，才会更新，这避免了冲突（我之前居然没有意识到）
-cp $i $TO_DIR$di/en.app.json -u
-done
-
-for i in `find store -iname 'applist.json' -type f`
-do
-di=${i%/*}
-cp $i $TO_DIR$di/applist.json -u  # -u 表示只有当源文件比目标文件新(或者目标文件不存在)时，才会更新，这避免了冲突（我之前居然没有意识到）
-done
-######################################################
-
-find aarch64-store -type d -exec mkdir -p $TO_DIR\{\} \;
-
-for i in `find aarch64-store -iname 'app.json' -type f`
-do
-di=${i%/*}
-cp $i $TO_DIR$di/app.json -u  # -u 表示只有当源文件比目标文件新(或者目标文件不存在)时，才会更新，这避免了冲突（我之前居然没有意识到）
-cp $i $TO_DIR$di/en.app.json -u
-done
-
-for i in `find aarch64-store -iname 'applist.json' -type f`
-do
-di=${i%/*}
-cp $i $TO_DIR$di/applist.json -u  # -u 表示只有当源文件比目标文件新(或者目标文件不存在)时，才会更新，这避免了冲突（我之前居然没有意识到）
-done
-
-
-
-echo "从仓库更新完毕"
 cd $TO_DIR
 
-#合并仓库生成的新json和来自gitee仓库的pr推送到gitee
 git add .
 git config user.name spark-bot
 git config user.email jifengshenmo@outlook.com
 git commit -m "Update Appinfo From Spark Repo..."
 git push origin
 
-# 同步到下游
-#rsync -rztP --delete-after /home/ftp/json/  spark@101.132.134.206::json --password-file=/etc/rsyncpasswd --exclude ".git*"
