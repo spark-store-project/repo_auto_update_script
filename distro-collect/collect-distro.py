e3d6dc686de3c9912609f925228bdeb4383cd3bb
shenmo <jifengshenmo@outlook.com>
2023-09-17 12:43:36 +0800

    distro

--- distro-collect/collect-distro.py
+++ distro-collect/collect-distro.py
@@ -15,17 +15,18 @@ def receive_info():
         # 提取Distributor ID和Release信息
         distributor_id = data.get('Distributor ID', '')
         release = data.get('Release', '')
+        architecture = data.get('Architecture', '')
 
         # 将信息追加到/root/result.csv文件中
         with open('/root/result.csv', mode='a', newline='') as csv_file:
-            fieldnames = ['Distributor ID', 'Release']
+            fieldnames = ['Distributor ID', 'Release', 'Architecture']
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
             # 如果文件不存在，写入表头
             if csv_file.tell() == 0:
                 writer.writeheader()
 
-            writer.writerow({'Distributor ID': distributor_id, 'Release': release})
+            writer.writerow({'Distributor ID': distributor_id, 'Release': release, 'Architecture': architecture})
 
         return 'Data received and written to /root/result.csv successfully', 200
     except Exception as e:
